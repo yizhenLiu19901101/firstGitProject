f7425825cbceaceb43e9e473532b529c88a2f732
yizhenLiu19901101 <3081583540@qq.com>
2018-05-16 11:06:54 +0800

    水仙花数问题

--- src/main/java/com/yizhen/demo/test/ShuiXianHuaTest.java
+++ src/main/java/com/yizhen/demo/test/ShuiXianHuaTest.java
@@ -1,9 +1,21 @@
 package com.yizhen.demo.test;
 
+/**
+ * 水仙花数的问题
+ * @author  liuyizhen
+ */
 public class ShuiXianHuaTest {
     public static final int NUMBER_LIMIT = 1000;
     public static final int POW_NUM = 3;
     public static void main(String[] args){
+        //水仙花数问题
+        shuiXianHua();
+    }
+
+    /**
+     * 得到水仙花数
+     */
+    public static void shuiXianHua(){
         for(int i=0;i<NUMBER_LIMIT;i++){
             if(i> 100){
                 int firstNum = i/100;
@@ -17,4 +29,3 @@ public class ShuiXianHuaTest {
         }
     }
 }
-
